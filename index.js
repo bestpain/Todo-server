7a913153702c7b3d87ab3ef2e3d4f1df38d24d12
nitish ranjan <36075994+bestpain@users.noreply.github.com>
2020-03-07 20:51:35 +0530

    Update index.js

--- index.js
+++ index.js
@@ -17,28 +17,29 @@ connection.connect(err=>{
 	}
 });
 
-CREATE TABLE `buckets` (
+connection.query(CREATE TABLE `buckets` (
   `id` int NOT NULL AUTO_INCREMENT,
   `category` varchar(50) NOT NULL,
   PRIMARY KEY (`id`)
 ) ;
+)
 
-ALTER TABLE buckets AUTO_INCREMENT=1; 
+connection.query(ALTER TABLE buckets AUTO_INCREMENT=1;) 
 
-CREATE TABLE tasks (
+connection.query(CREATE TABLE tasks (
     id int NOT NULL AUTO_INCREMENT,
     task varchar(50) NOT NULL,
     isComplete bool,
     bucketid int not null,
     PRIMARY KEY (id),
 	FOREIGN KEY (bucketid) REFERENCES buckets(id) ON DELETE CASCADE
-);
+);)
 
-ALTER TABLE tasks AUTO_INCREMENT=100; 
+// ALTER TABLE tasks AUTO_INCREMENT=100; 
 
-INSERT INTO buckets (category) value ('College');
-INSERT INTO buckets (category) value ('Home');
-INSERT INTO buckets (category) value ('Work');
+// INSERT INTO buckets (category) value ('College');
+// INSERT INTO buckets (category) value ('Home');
+// INSERT INTO buckets (category) value ('Work');
 
 
 app.use(bodyParser.json());
