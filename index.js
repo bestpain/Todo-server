fcd883f5f315d43806103c578a8953c0319c6c96
nitish ranjan <36075994+bestpain@users.noreply.github.com>
2020-03-07 20:26:01 +0530

    Update index.js

--- index.js
+++ index.js
@@ -6,13 +6,9 @@ const bodyParser=require('body-parser')
 const port=process.env.PORT || 8000;
 
 
-const connection=mysql.createConnection({
-	//mysql://b060476830577f:8462c951@us-cdbr-iron-east-04.cleardb.net/heroku_edb32773ac4caee?reconnect=true
-		host:'us-cdbr-iron-east-04.cleardb.net/heroku_edb32773ac4caee?reconnect=true',
-		user:'b060476830577f',
-		database:'todo',
-		password:'8462c951',
-	});
+const connection=mysql.createConnection(
+	mysql://b060476830577f:8462c951@us-cdbr-iron-east-04.cleardb.net/heroku_edb32773ac4caee?reconnect=true
+	);
 //console.log(connection)
 
 connection.connect(err=>{
