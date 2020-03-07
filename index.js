427b1e2787b4d56fb6f66f5e68618bba79158297
nitish ranjan <36075994+bestpain@users.noreply.github.com>
2020-03-07 21:04:47 +0530

    Update index.js

--- index.js
+++ index.js
@@ -17,32 +17,6 @@ connection.connect(err=>{
 	}
 });
 
-const create_table="CREATE TABLE `buckets` (
-  `id` int NOT NULL AUTO_INCREMENT,
-  `category` varchar(50) NOT NULL,
-  PRIMARY KEY (`id`)
-) ;"
-
-connection.query(create_table,(err,res)=>{
-	console.log(err,res)})
-
-// connection.query(ALTER TABLE buckets AUTO_INCREMENT=1;) 
-
-// connection.query(CREATE TABLE tasks (
-//     id int NOT NULL AUTO_INCREMENT,
-//     task varchar(50) NOT NULL,
-//     isComplete bool,
-//     bucketid int not null,
-//     PRIMARY KEY (id),
-// 	FOREIGN KEY (bucketid) REFERENCES buckets(id) ON DELETE CASCADE
-// );)
-
-// ALTER TABLE tasks AUTO_INCREMENT=100; 
-
-// INSERT INTO buckets (category) value ('College');
-// INSERT INTO buckets (category) value ('Home');
-// INSERT INTO buckets (category) value ('Work');
-
 
 app.use(bodyParser.json());
 app.use(cors());
